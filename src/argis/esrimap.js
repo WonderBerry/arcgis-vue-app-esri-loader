0760b5273dd52c6c33aa863cc054e116243b37d6
Andrey Solovyov <56723639+WonderBerry@users.noreply.github.com>
2022-05-12 23:30:02 +0300

    master initial

---
+++ src/argis/esrimap.js
@@ -0,0 +1,69 @@
+import Map from "@arcgis/core/Map";
+import Ground from "@arcgis/core/Ground";
+import MapView from "@arcgis/core/views/MapView";
+import SceneView from "@arcgis/core/views/SceneView";
+import SceneLayer from "@arcgis/core/layers/SceneLayer";
+
+export const createMap = () => {
+  return new Map({
+    basemap: "topo-vector",
+    ground: new Ground({
+      navigationConstraint: "stay-above",
+    }),
+  });
+};
+// from master
+
+export const createMap2D = (map, initialParam) => {
+  const view = new MapView({
+    map: map,
+    container: initialParam.el,
+    zoom: 8,
+    center: [-118, 34],
+    //   ui: {
+    //     components: [],
+    //   },
+    //   constraints: {
+    //     minZoom,
+    //     maxZoom,
+    //   },
+  });
+
+  return view;
+};
+
+export const createMap3D = (map, initialParam) => {
+  const view = new SceneView({
+    map: map,
+    container: initialParam.el,
+    zoom: 8,
+    center: [-118, 34],
+    //   ui: {
+    //     components: [],
+    //   },
+    //   qualityProfile: 'low',
+    //   environment: {
+    //     background: {
+    //       type: 'color',
+    //       color: [0, 0, 0, 1]
+    //     },
+    //     starsEnabled: false,
+    //     atmosphereEnabled: false,
+    //     lighting: {
+    //       date: new Date("June 22, 2019 04:00:00 UTC"),
+    //       directShadowsEnabled: false,
+    //       ambientOcclusionEnabled: false,
+    //     }
+    //   }
+  });
+
+  return view;
+};
+
+export const createSceneLayer = (params) => {
+  const sl = new SceneLayer({
+    url: params.url,
+  });
+  console.log("sl", sl);
+  return sl;
+};
